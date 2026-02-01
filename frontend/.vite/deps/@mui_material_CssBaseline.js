da40137318bc757e1b8ef67bc4f23a7697e3002e
Henri Bonamy <henribonamy@gmail.com>
2026-02-01 18:35:04 +0100

    removing .vite files

--- frontend/.vite/deps/@mui_material_CssBaseline.js
+++
@@ -1,9 +0,0 @@
-import {
-  CssBaseline_default
-} from "./chunk-TEIF6AVW.js";
-import "./chunk-Z2HTA7RG.js";
-import "./chunk-DDNM7ENY.js";
-export {
-  CssBaseline_default as default
-};
-//# sourceMappingURL=@mui_material_CssBaseline.js.map
